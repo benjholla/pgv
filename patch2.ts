76dfbd7a41f4f6f3f25dd246b5d047f56aa47668
Ben Holland <benjholla@gmail.com>
2026-06-30 09:19:12 -0500

    Remove temp file

--- patch2.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-let code = readFileSync('src/model.ts', 'utf8');
-code = code.replace(
-  'if (lower.includes("javascript:") || lower.includes("vbscript:") || lower.includes("data:text/html")) {',
-  'if (lower.includes("javascript:") || lower.includes("vbscript:") || lower.includes("data:text/html")) {'
-);
-writeFileSync('src/model.ts', code);
